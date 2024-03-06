bdae029c677e92721131acffa43c820b4a167c0f
KimCookieYa <min49590@gmail.com>
2024-03-07 00:08:31 +0900

    feat: 개발 테스트 중에는 테스트계정 사용하도록 세팅

--- src/stores/useUserAccount.ts
+++ src/stores/useUserAccount.ts
@@ -7,6 +7,13 @@ interface UserAccountState {
 }
 
 export const useUserAccount = create<UserAccountState>(set => ({
-  userAccount: null,
+  userAccount: import.meta.env.VITE_DEV_MODE
+    ? ({
+        seedPhrase: import.meta.env.VITE_TESTNET_TEST_ACCOUNT_SEED_PHRASE,
+        publicKey: import.meta.env.VITE_TESTNET_TEST_ACCOUNT_PUBLIC_KEY,
+        secretKey: import.meta.env.VITE_TESTNET_TEST_ACCOUNT_SECRET_KEY,
+        accountId: import.meta.env.VITE_TESTNET_TEST_ACCOUNT_ID,
+      } as UserAccount)
+    : null,
   setUserAccount: userAccount => set({ userAccount }),
 }));
